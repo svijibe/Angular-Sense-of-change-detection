b701c23571d69482a777c06736afbb1dfdf63d45
VijiSubra <viji.subramanyan@yahoo.com>
2025-05-01 12:31:10 -0400

    Change detection Async

--- src/app/messages/messages-list/messages-list.component.ts
+++ src/app/messages/messages-list/messages-list.component.ts
@@ -1,38 +1,44 @@
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, DestroyRef, inject, input, OnInit } from '@angular/core';
 import { MessagesService } from '../messages.service';
 import { subscribeOn } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-messages-list',
   standalone: true,
+  imports: [AsyncPipe],
   templateUrl: './messages-list.component.html',
   styleUrl: './messages-list.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush 
 })
-export class MessagesListComponent implements OnInit{
+export class MessagesListComponent {
 
   //messages = input.required<string[]>();
   private messagesService = inject (MessagesService);
 
   //to run change detection manually
-  private cdRef = inject(ChangeDetectorRef);
-  private destroyRef = inject (DestroyRef);
+  // private cdRef = inject(ChangeDetectorRef);
+  // private destroyRef = inject (DestroyRef);
 
-  // messages = this.messagesService.allMessages;
-  // get messages() {
-  //   return this.messagesService.allMessages;
+  // // messages = this.messagesService.allMessages;
+  // // get messages() {
+  // //   return this.messagesService.allMessages;
+  // // }
+  // messages: string[] = [];
+
+  // ngOnInit(): void {
+  //   const subscription =  this.messagesService.messages$.subscribe((messages) => {
+  //       this.messages = messages;
+  //     this.cdRef.markForCheck(); //Triggering change detection manually
+  //   });
+  //   this.destroyRef.onDestroy(()=>{
+  //     subscription.unsubscribe();
+  //   });
   // }
-  messages: string[] = [];
 
-  ngOnInit(): void {
-    const subscription =  this.messagesService.messages$.subscribe((messages) => {
-        this.messages = messages;
-      this.cdRef.markForCheck(); //Triggering change detection manually
-    });
-    this.destroyRef.onDestroy(()=>{
-      subscription.unsubscribe();
-    });
-  }
+  private messageService = inject(MessagesService);
+  messages$ = this.messagesService.messages$;
+
   get debugOutput() {
     console.log('[MessagesList] "debugOutput" binding re-evaluated.');
     return 'MessagesList Component Debug Output';
